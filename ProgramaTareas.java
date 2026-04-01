fe38524987546de4cc975b3e5235c4c80a1c68d0
danielernesto227-sudo <danielernesto227@gmail.com>
2026-04-01 12:30:18 -0500

    mas condiciones

--- ProgramaTareas.java
+++ ProgramaTareas.java
@@ -2,8 +2,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class ProgramaTareas {
     public static void main(String[] args) {
-        ArrayList<String>taskhomenext= new ArrayList<>();
-        ArrayList<String>completedtask= new ArrayList<>();
+        ArrayList<String>taskHomeNext= new ArrayList<>();
+        ArrayList<String>completedTask= new ArrayList<>();
         Scanner sc= new Scanner(System.in);
         System.out.println("***Bienvenido a la lista de tareas***\n"+
         "Cual es tu nombre estimado(a): ");
@@ -11,131 +11,139 @@ public class ProgramaTareas {
         System.out.println("Que tal estimado " + user +"."+"\nAtiende nuestras opciones y sigue las indicaciones...\n");
 
         boolean keep = true;
-        while(keep)
-            {
+        while(keep){
             System.out.println("***Menu opciones***");
             System.out.println("1.Agregar tareas\n"+"2.Mostrar tareas pendientes "+"\n3.mostrar tareas completadas"
-             +"\n4.Eliminar tareas "+"\n5.Marcar tareas completadas"+"\n6.Salir"  
-            );
+             +"\n4.Eliminar tareas "+"\n5.Marcar tareas completadas"+"\n6.Salir");
             System.out.println("Elije una opcion: ");
             int select = sc.nextInt();
             sc.nextLine();
             switch (select) {
                 case 1:
-                    addTask(taskhomenext, sc);
+                    addTask(taskHomeNext, sc);
                     break;
                 case 2: 
-                    lookpendienTask(taskhomenext);
+                    lookPendienTask(taskHomeNext);
                     break;
                 case 3:
-                    lookcompletetask(completedtask);
+                    lookcompleteTask(completedTask);
                     break;
                 case 4:
-                    droptaskhomenext (taskhomenext, sc);
+                    dropTaskHomeNext (taskHomeNext, sc);
                     break;
                 case 5:
-                    changesFuntion(taskhomenext, completedtask, sc);
+                    changesFuntion(taskHomeNext, completedTask, sc);
                     break;
                 case 6:
                     keep = false;
                     System.out.println("Gracias por hacer uso de esta app. Hasta la proxima ");
                     break;
-                    default:
+                default:
                     System.out.println("Opcion no encontrada"+"Imgresa una de las opciones disponibles");
                     break;
             }
-        }
+        } // cierre while
     }  // cierre de main
 
-    public static ArrayList <String> addTask (ArrayList <String> taskhomenext, Scanner sc){
+    public static ArrayList <String> addTask (ArrayList <String> taskHomeNext, Scanner sc){
         boolean keep=true;
         while(keep){
-        System.out.println("ingresa el numero de tareas a cumplir.");
-        int times = sc.nextInt();
-        sc.nextLine(); // limpiar el buffer
-        for(int i = 0; i<times;++i){
-            System.out.println("Tarea # "+ (i+1)+ ": ");
-            String list= sc.nextLine();
-            taskhomenext.add(list);
-          
-        }
+            System.out.println("ingresa el numero de tareas a cumplir.");
+            int times = sc.nextInt();
+            sc.nextLine(); // limpiar el buffer
+            for(int i = 0; i<times;++i){
+                System.out.println("Tarea # "+ (i+1)+ ": ");
+                String list= sc.nextLine();
+                taskHomeNext.add(list);
+            }
             System.out.println("Deseas agregar mas tareas?(si) ");
-           String repeat = sc.nextLine();
-           if(repeat.equalsIgnoreCase("si")){
-            keep=true;
-           }else{
-            keep = false;
-            System.out.println("Volvemos al menu principal");
+            String repeat = sc.nextLine();
+            if(repeat.equalsIgnoreCase("si")){
+                keep=true;
+            }else{
+                keep = false;
+                System.out.println("Volvemos al menu principal");
            }  
-      
       }
-
-    return taskhomenext;
-}
-
-    public static void lookpendienTask(ArrayList<String>taskhomenext){
-     if(taskhomenext.size()>0){
-    System.out.println("Su lista de tareas pendientes es: ");
-    for(String must : taskhomenext){
-    System.out.println( "Tarea : "+ must);
+        return taskHomeNext;
     }
-     } else if(taskhomenext.size()==0){
-        System.out.println("Estas al dia en tus tareas");
+
+    public static void lookPendienTask(ArrayList<String>taskHomeNext){
+     if(taskHomeNext.size()>0){
+        System.out.println("Su lista de tareas pendientes es: ");
+            for(String must : taskHomeNext){
+                System.out.println( "Tarea : "+ must);
+            }
+     } else if(taskHomeNext.size()==0){
+            System.out.println("Estas al dia en tus tareas");
+       }
      }
-     }
-     public static void lookcompletetask(ArrayList<String>completedtask){
+     public static void lookcompleteTask(ArrayList<String>completedTask){
         boolean keep = true;
         while (keep) {
             
         
-        if(completedtask.size()>0){
-        System.out.println("Las tareas comletadas son las siguientes: ");
-        for(String must : completedtask){
-            System.out.println("Tarea : " + must + " = " + "✓");
-            keep = false;
-        }
-        }else if (completedtask.size() ==0){
-        System.out.println("Estas un poco fresco.\n"+ "\nEs un deber ponerte al dia");
-        keep=false;
+            if(completedTask.size()>0){
+                System.out.println("Las tareas comletadas son las siguientes: ");
+                    for(String must : completedTask){
+                        System.out.println("Tarea : " + must + " = " + "✓");
+                        keep = false;
+                    }
+        }else if (completedTask.size() ==0){
+            System.out.println("Estas un poco fresco.\n"+ "\nEs un deber ponerte al dia");
+            keep=false;
         }
         }
     }
-    public static ArrayList<String>  droptaskhomenext (ArrayList<String>hometasknext, Scanner sc){
+
+    public static ArrayList<String>  dropTaskHomeNext (ArrayList<String>homeTaskNext, Scanner sc){
         boolean keep = true;
         while(keep){
-        System.out.println("Deseas eliminar alguna tareas? indica cual eliminaras: ");
-        String dropTask= sc.nextLine();
-        if(hometasknext.contains(dropTask)){
-            hometasknext.remove(dropTask);
+           if(homeTaskNext.size()==0){
+            System.out.println("No se pueden eliminar tareas, lista vacia");
+            break;
+           }
+           System.out.println("Que tarea deseas eliminar?"+"\nIngresala: ");
+           String dropTask = sc.nextLine();
+           if(homeTaskNext.contains(dropTask)){
+                homeTaskNext.remove(dropTask);
+                System.out.println("La tarea "+ dropTask +" ha sido eliminada");
+           }else{
+                    System.out.println("Esa tarea no existe");
+                 }
 
-        } 
-         if(hometasknext.size()>0){
-            System.out.println("Quieres eliminar mas tareas? (si)");
+           if(homeTaskNext.size()>0){
+            System.out.println("Quieres eliminar alguna otra tarea?(si/no)");
             String repeat = sc.nextLine();
-        if(repeat.equalsIgnoreCase("si")){
-            keep = true;
-        }else{
-            System.out.println("Ya no tienes tareas por eliminar");
-            keep = false;
-                }
+            if(!repeat.equalsIgnoreCase("si")){
+                keep = false;
+
+            }else{
+                System.out.println("No hay mas tareas por eliminar");
+                keep = false;
+            }
+            }
+            }
+        
+            
+           
+           return homeTaskNext;
         }
-        }
-        return hometasknext;
-    }
-        public static void changesFuntion(ArrayList <String> taskhomenext, ArrayList<String> completedtask,Scanner sc){
+    
+        public static void changesFuntion(ArrayList <String> taskHomeNext, ArrayList<String> completedTask,Scanner sc){
         boolean keep = true;
         while(keep){
             System.out.println("Muestra las tareas que has cumplido: ");
             String list = sc.nextLine();
-            if(!taskhomenext.contains(list)){
+            if(!taskHomeNext.contains(list)){
                 System.out.println("Esta tarea no esta pendiente y no se puede marcar como completada");
-            }else if(taskhomenext.contains(list) && ! completedtask.contains(list)){
-                taskhomenext.remove(list);
-                completedtask.add(list);
+            }else if(taskHomeNext.contains(list) && ! completedTask.contains(list)){
+                taskHomeNext.remove(list);
+                completedTask.add(list);
                 System.out.println("La tarea" + list + "ha sido actualizada, ahora paso a estar completada");
 
             }
-            if(taskhomenext.size()==0){
+            if(taskHomeNext.size()==0){
              System.out.println("Ya no quedan mas tareas a marcar");
              return;
             }
